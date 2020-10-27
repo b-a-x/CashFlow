29e0b6836141dfdcdd37bc6faa2d7caf6ad1918d
b_a_x <ily-odinokov@yandex.ru>
2020-10-27 19:10:43 +0300

    use refresh token

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -20,6 +20,8 @@ namespace WebClient
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<RefreshTokenService>();
+            builder.Services.AddScoped<HttpInterceptorService>();
 
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddAuthorizationCore();
---
+++ src/WebClient/Services/RefreshTokenService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace WebClient.Services
+{
+    public class RefreshTokenService
+    {
+        private readonly AuthenticationStateProvider _authProvider;
+        private readonly IAuthenticationService _authService;
+
+        public RefreshTokenService(AuthenticationStateProvider authProvider, IAuthenticationService authService)
+        {
+            _authProvider = authProvider;
+            _authService = authService;
+        }
+
+        public async Task<string> TryRefreshToken()
+        {
+            AuthenticationState authState = await _authProvider.GetAuthenticationStateAsync();
+            ClaimsPrincipal user = authState.User;
+
+            string exp = user.FindFirst(c => c.Type.Equals("exp")).Value;
+            DateTimeOffset expTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(exp));
+            if ((expTime - DateTime.UtcNow).TotalMinutes <= 2)
+                return await _authService.RefreshToken();
+
+            return string.Empty;
+        }
+	}
+}
